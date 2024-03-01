b5d22a246f6112bcc7938528b04c6927b4c32003
navnath13 <navnathdoke2222@gmail.com>
2024-03-01 03:40:32 -0800

    user url change to users

--- userservice/src/main/java/com/skbit/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/skbit/userservice/UserserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController("/user")
+@RestController("/users")
 public class UserserviceApplication {
 	
 	@GetMapping("/")
