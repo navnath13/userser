29f82a1f216b056a4790fc646bb3f4c42213cddb
navnath13 <navnathdoke2222@gmail.com>
2024-03-01 03:13:44 -0800

    message is added

--- userservice/src/main/java/com/skbit/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/skbit/userservice/UserserviceApplication.java
@@ -2,10 +2,17 @@ package com.skbit.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/user")
 public class UserserviceApplication {
-
+	
+	@GetMapping("/")
+    public String getMessage() {
+    	return "this is project is deployed ond server";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
